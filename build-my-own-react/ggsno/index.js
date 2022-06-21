b33a7b9ffcc8b6df6e2e5fb11e1cfaf21fd68075
ks <ogs0518@gmail.com>
2022-06-22 00:59:07 +0900

    feat: step 4. Fibers

--- build-my-own-react/ggsno/index.js
+++ build-my-own-react/ggsno/index.js
@@ -1,74 +1,18 @@
-function createElement(type, props, ...children) {
-  return {
-    type,
-    props: {
-      ...props,
-      children: children.map((child) =>
-        typeof child === "object" ? child : createTextElement(child)
-      ),
-    },
-  };
-}
+import MyReact from "./MyReact.js";
 
-function createTextElement(text) {
-  return {
-    type: "TEXT_ELEMENT",
-    props: {
-      nodeValue: text,
-      children: [],
-    },
-  };
-}
-
-function render(element, container) {
-  const dom =
-    element.type == "TEXT_ELEMENT"
-      ? document.createTextNode("")
-      : document.createElement(element.type);
-
-  const isProperty = (key) => key !== "children";
-  Object.keys(element.props)
-    .filter(isProperty)
-    .forEach((name) => {
-      dom[name] = element.props[name];
-    });
-
-  element.props.children.forEach((child) => render(child, dom));
-
-  container.appendChild(dom);
-}
-
-let nextUnitOfWork = null
-​
-function workLoop(deadline) {
-  let shouldYield = false
-  while (nextUnitOfWork && !shouldYield) {
-    nextUnitOfWork = performUnitOfWork(
-      nextUnitOfWork
-    )
-    shouldYield = deadline.timeRemaining() < 1
-  }
-  requestIdleCallback(workLoop)
-}
-​
-requestIdleCallback(workLoop)
-​
-function performUnitOfWork(nextUnitOfWork) {
-  // TODO
-}
-​
-
-const Didact = {
-  createElement,
-  render,
-};
-
-/** @jsx Didact.createElement */
-const element = (
-  <div id="foo">
-    <a>bar</a>
-    <b />
-  </div>
+const element = MyReact.createElement(
+  "div",
+  { id: "test" },
+  MyReact.createElement(
+    "h1",
+    null,
+    "h1 child of root",
+    MyReact.createElement("p", null, "p child of h1"),
+    MyReact.createElement("a", null, "a sibling of p")
+  ),
+  MyReact.createElement("h2", { style: "color: #00ff00" }, "h2 sibling of h1")
 );
+
 const container = document.getElementById("root");
-Didact.render(element, container);
+
+MyReact.render(element, container);
